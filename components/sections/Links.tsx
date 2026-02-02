18f2bbe69e9ae9ae105ba5ee0d21816a57e2d7fc
tadanobutaaaaa <youyah1004@gmail.com>
2026-02-02 16:15:39 +0900

    Linksセクションを更新し、サービスのリストを動的に表示するように変更しました。未使用のテキストを削除し、各サービスにリンクを追加しました。

--- components/sections/Links.tsx
+++ components/sections/Links.tsx
@@ -1,12 +1,23 @@
 export default function Links() {
+    const services = [
+        { name: "X", url: "https://x.com/P_tdn2" },
+        { name: "Qiita", url: "https://qiita.com/tadanobutaaaaa" },
+        { name: "GitHub", url: "https://github.com/tadanobutaaaaa" },
+        { name: "VRChat", url: "https://vrchat.com/home/user/usr_97e9b0f8-b131-4b0f-b9e4-3a1a02805ad7" },
+    ];
     return (
         <div className="flex flex-col items-center gap-30">
             <h2 className="text-4xl font-bold">Links</h2>
             <div>
-                <p>X</p>
-                <p>Qiita</p>
-                <p>github</p>
-                <p>vrchat</p>
+                <ul className="list-disc list-inside">
+                    {services.map((service) => (
+                        <li key={service.name}>
+                            <a href={service.url} target="_blank" rel="noopener noreferrer" className="text-[#2ca9e1] hover:underline">
+                                {service.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
